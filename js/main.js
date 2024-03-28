40a445ca6491e32fb38e840f9ca86aaf5e471244
smaq92 <145607578+smaq92@users.noreply.github.com>
2024-03-28 12:00:21 -0600

    made it responsive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 let btn = document.querySelector(".toggle-btn");
-let homeBtn = document.querySelector(".home-btn")
 let headerNav = document.querySelector("header");
-let headerLogo = document.querySelector(".logo");
-let popup = document.querySelector(".popup");
-let close = document.querySelector(".close");
 let headerNavItem = document.querySelector(".home-nav");
 
 btn.addEventListener("click", ()=>{
@@ -15,25 +11,4 @@ headerNavItem.addEventListener("click", ()=>{
     headerNav.classList.remove("show-nav");
     console.log("link is clicked")
 })
-
-// headerLogo.addEventListener("mouseover", (event)=>{
-//     event.target.src="img/vertical-logo-name.png";
-// })
-
-// headerLogo.addEventListener("mouseout", (event)=>{
-//     event.target.src="img/horizontal-logo.png";
-// })
-
-
-// homeBtn.addEventListener("mouseover", (event)=>{
-//     event.target.src="img/vertical-logo-name.png";
-// })
-
-// homeBtn.addEventListener("mouseout", (event)=>{
-//     event.target.src="img/horizontal-logo.png";
-// })
-
-// homeBtn.addEventListener("click", (event)=>{
-//     location.href = "home.html";
-// })
 
