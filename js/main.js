7314511a54b422036e31bc47fd7a55469c2a78d8
smaq92 <145607578+smaq92@users.noreply.github.com>
2024-03-28 10:53:05 -0600

    Made it responsive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 let btn = document.querySelector(".toggle-btn");
-let headerNav = document.querySelector(".home-nav");
-
-btn.addEventListener("mouseover", (event)=>{
-    event.target.src="img/vertical-logo-name.png";
-})
-
-btn.addEventListener("mouseout", (event)=>{
-    event.target.src="img/horizontal-logo.png";
-})
+let homeBtn = document.querySelector(".home-btn")
+let headerNav = document.querySelector("header");
+let headerLogo = document.querySelector(".logo");
+let popup = document.querySelector(".popup");
+let close = document.querySelector(".close");
+let headerNavItem = document.querySelector(".home-nav");
 
 btn.addEventListener("click", ()=>{
-    location.href = "home.html";
+    headerNav.classList.toggle("show-nav");
+    }
+)
+
+headerNavItem.addEventListener("click", ()=>{
+    headerNav.classList.remove("show-nav");
+    console.log("link is clicked")
 })
+
+// headerLogo.addEventListener("mouseover", (event)=>{
+//     event.target.src="img/vertical-logo-name.png";
+// })
+
+// headerLogo.addEventListener("mouseout", (event)=>{
+//     event.target.src="img/horizontal-logo.png";
+// })
+
+
+// homeBtn.addEventListener("mouseover", (event)=>{
+//     event.target.src="img/vertical-logo-name.png";
+// })
+
+// homeBtn.addEventListener("mouseout", (event)=>{
+//     event.target.src="img/horizontal-logo.png";
+// })
+
+// homeBtn.addEventListener("click", (event)=>{
+//     location.href = "home.html";
+// })
+
